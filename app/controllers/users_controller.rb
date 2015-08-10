8faa77fd6534ed7a0ae95aebe0f57497ce23e96e
michaelkunc <mjkunc@gmail.com>
2015-08-10 14:01:30 -0500

    new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect "/"
+      redirect_to "/"
     else
       @errors = @user.errors
       render 'users/new'
     end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :image_url, :bio, :password)
   end
 
 end
