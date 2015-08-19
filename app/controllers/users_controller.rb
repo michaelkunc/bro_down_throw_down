9ed2ad4d7b2522f54957c3f160bbe9f523871cd1
michaelkunc <mjkunc@gmail.com>
2015-08-19 11:47:12 -0500

    rescind challenge button added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,10 +52,5 @@ before_action :logged_in_user, only: [:edit, :update, :destroy, :challenged, :ch
     Relationship.where(challenger: user)
   end
 
-  def logged_in_user
-    unless logged_in?
-      redirect_to login_url
-    end
-  end
 
 end
